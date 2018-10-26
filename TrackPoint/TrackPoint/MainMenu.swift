52fe88e25c585ed657f05d79017b6b2d5876fe49
taylortraviss <ttraviss@sfu.ca>
2018-10-26 15:07:53 -0700

    First Commit

---
+++ TrackPoint/TrackPoint/MainMenu.swift
@@ -0,0 +1,30 @@
+//
+//  MainMenuViewController.swift
+//  TrackPoint
+//
+//  Created by Taylor Traviss on 2018-10-24.
+//  Copyright © 2018 Pit Bulls. All rights reserved.
+//
+
+import UIKit
+
+class MainMenu: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
