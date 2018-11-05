7adf8527935703f16beb79c5e31b9098134ce88f
Cercospora <qha20@sfu.ca>
2018-11-05 13:25:32 -0800

    Update Progress.swift

--- TrackPoint/TrackPoint/Progress.swift
+++ TrackPoint/TrackPoint/Progress.swift
@@ -1,8 +1,6 @@
+// File: Progress.swift
+// Authors: Taylor Traviss
 //
-//  Progress.swift
-//  TrackPoint
-//
-//  Created by Taylor Traviss on 2018-10-25.
 //  Copyright © 2018 Pit Bulls. All rights reserved.
 //
 
