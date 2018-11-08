215957850fcab91ab14656f1c264847140a360d5
Manhas <manhas@imac.hitronhub.home>
2018-11-07 21:01:55 -0800

    initial AR setup

--- TrackPoint/TrackPoint/GameScreen.swift
+++ TrackPoint/TrackPoint/GameScreen.swift
@@ -5,27 +5,27 @@
 //
 
 import UIKit
+import ARKit
 
 class GameScreen: UIViewController  {
     //MARK: Variables
     let collector:DataRun = DataRun()
-       fileprivate var sensor_timer: Timer!
     let gameComplete = GameComplete()
+    let ARconfig = ARWorldTrackingConfiguration()
 
     //MARK: Outlets
     @IBOutlet weak var StartButton: UIButton!
     @IBOutlet weak var StopButton: UIButton!
-    @IBOutlet weak var accelX: UILabel!
-    @IBOutlet weak var accelY: UILabel!
-    @IBOutlet weak var accelZ: UILabel!
-    @IBOutlet weak var rotateX: UILabel!
-    @IBOutlet weak var rotateY: UILabel!
-    @IBOutlet weak var rotateZ: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         StopButton.isEnabled = false
+        
+        //enabling debug options for sceneView for now... nothing but camera view with capabilities of AR framework
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        print("Set debug options!")
     }
     
     override func didReceiveMemoryWarning() {
@@ -38,34 +38,16 @@ class GameScreen: UIViewController  {
         StopButton.isEnabled = true
         StartButton.isEnabled = false
         collector.start()
-        sensor_timer = Timer.scheduledTimer(timeInterval: 1/60, target: self, selector: #selector(self.updateLabels), userInfo: nil, repeats: true)
+        sceneView.session.run(ARconfig) //start the AR session
     }
 
     // stop data recording and label update timer
     @IBAction func StopClicked(_ sender: Any) {
         collector.end()
-        sensor_timer.invalidate();
+        sceneView.session.pause() //pauses processing in the session
         //performSegue(withIdentifier: "StopSegue", sender: self)
         //self.present(gameComplete, animated: true, completion: nil)
     }
     
-    // timerfunc updates labels with current sensor data
-    @objc fileprivate  func updateLabels(){
-        let curr_data = collector.get_last_entry();
-        let aX = curr_data[0].0
-        let aY = curr_data[0].1
-        let aZ = curr_data[0].2
-        let rX = curr_data[1].0
-        let rY = curr_data[1].1
-        let rZ = curr_data[1].2
-        //Updates the labels
-        self.accelX.text = String(format:"%6.3lf", aX)
-        self.accelY.text = String(format:"%6.3lf", aY)
-        self.accelZ.text = String(format:"%6.3lf", aZ)
-        self.rotateX.text = String(format:"%6.3lf", rX)
-        self.rotateY.text = String(format:"%6.3lf", rY)
-        self.rotateZ.text = String(format:"%6.3lf", rZ)
-        //print(curr_data[0])
-    }
     
 }
