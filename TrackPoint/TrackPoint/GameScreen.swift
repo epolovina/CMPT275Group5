5efc08a1ed51f33f850ef53773b9191ef405b911
Khalil Ammar <khalilammar@khalils-mac.local>
2018-11-13 00:08:17 -0800

    Testing AR with death star asset

--- TrackPoint/TrackPoint/GameScreen.swift
+++ TrackPoint/TrackPoint/GameScreen.swift
@@ -5,27 +5,27 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
 
-class GameScreen: UIViewController  {
+class GameScreen: UIViewController {
     //MARK: Variables
+    //let objNode = SCNNode()
+    private let objNode = SCNNode()
     let collector:DataRun = DataRun()
-       fileprivate var sensor_timer: Timer!
+    fileprivate var sensor_timer: Timer!
     let gameComplete = GameComplete()
 
     //MARK: Outlets
-    @IBOutlet weak var StartButton: UIButton!
-    @IBOutlet weak var StopButton: UIButton!
-    @IBOutlet weak var accelX: UILabel!
-    @IBOutlet weak var accelY: UILabel!
-    @IBOutlet weak var accelZ: UILabel!
-    @IBOutlet weak var rotateX: UILabel!
-    @IBOutlet weak var rotateY: UILabel!
-    @IBOutlet weak var rotateZ: UILabel!
+    
+    @IBOutlet weak var sceneView: ARSCNView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        StopButton.isEnabled = false
+        let config = ARWorldTrackingConfiguration()
+        sceneView.session.run(config)
+        GenerateObject()
     }
     
     override func didReceiveMemoryWarning() {
@@ -33,39 +33,30 @@ class GameScreen: UIViewController  {
         // Dispose of any resources that can be recreated.
     }
     
-    // start data recording and label update timer
-    @IBAction func StartClicked(_ sender: Any) {
-        StopButton.isEnabled = true
-        StartButton.isEnabled = false
-        collector.start()
-        sensor_timer = Timer.scheduledTimer(timeInterval: 1/60, target: self, selector: #selector(self.updateLabels), userInfo: nil, repeats: true)
-    }
 
-    // stop data recording and label update timer
-    @IBAction func StopClicked(_ sender: Any) {
-        collector.end()
-        sensor_timer.invalidate();
-        //performSegue(withIdentifier: "StopSegue", sender: self)
-        //self.present(gameComplete, animated: true, completion: nil)
+    
+    func GenerateObject(){
+        let objNode = SCNNode()
+        objNode.position = SCNVector3(0,0,2)
+        guard let virtualObjScene = SCNScene(named: "deathStar.scn", inDirectory: "Models.scnassets/deathStar")
+            else{
+                return
+        }
+        
+        let wrapperNode = SCNNode()
+        
+        for child in virtualObjScene.rootNode.childNodes{
+            child.geometry?.firstMaterial?.lightingModel = .physicallyBased
+            wrapperNode.addChildNode(child)
+            
+        }
+        objNode.addChildNode(wrapperNode)
+        sceneView.scene.rootNode.addChildNode(objNode)
+        
+        
     }
     
-    // timerfunc updates labels with current sensor data
-    @objc fileprivate  func updateLabels(){
-        let curr_data = collector.get_last_entry();
-        let aX = curr_data[0].0
-        let aY = curr_data[0].1
-        let aZ = curr_data[0].2
-        let rX = curr_data[1].0
-        let rY = curr_data[1].1
-        let rZ = curr_data[1].2
-        //Updates the labels
-        self.accelX.text = String(format:"%6.3lf", aX)
-        self.accelY.text = String(format:"%6.3lf", aY)
-        self.accelZ.text = String(format:"%6.3lf", aZ)
-        self.rotateX.text = String(format:"%6.3lf", rX)
-        self.rotateY.text = String(format:"%6.3lf", rY)
-        self.rotateZ.text = String(format:"%6.3lf", rZ)
-        //print(curr_data[0])
-    }
+  
+    
     
 }
