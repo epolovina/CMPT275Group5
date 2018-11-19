f81b68b14fcc6f31d82d206bf29224a026c16ecd
Eddie <zhengedd@gmail.com>
2018-11-18 19:54:11 -0800

    AR+app

--- TrackPoint/TrackPoint/GameScreen.swift
+++ TrackPoint/TrackPoint/GameScreen.swift
@@ -5,28 +5,102 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
 
-class GameScreen: UIViewController  {
+class GameScreen: UIViewController {
     //MARK: Variables
     let collector:DataRun = DataRun()
-       fileprivate var sensor_timer: Timer!
     let gameComplete = GameComplete()
-
+    let ARconfig = ARWorldTrackingConfiguration()
+    //let ARconfig = AROrientationTrackingConfiguration()
+    
     //MARK: Outlets
-    @IBOutlet weak var StartButton: UIButton!
     @IBOutlet weak var StopButton: UIButton!
-    @IBOutlet weak var accelX: UILabel!
-    @IBOutlet weak var accelY: UILabel!
-    @IBOutlet weak var accelZ: UILabel!
-    @IBOutlet weak var rotateX: UILabel!
-    @IBOutlet weak var rotateY: UILabel!
-    @IBOutlet weak var rotateZ: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var readyButton: UIButton!
+    @IBOutlet weak var instructionScreen: UIView!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    @IBAction func readyButton(_ sender: Any) {
+        startTimer()
+        playBGM() //music
+        collector.start() //collect data api
+        instructionScreen.isHidden = true;
+        readyButton.isHidden = true;
+        sceneView.session.run(ARconfig) //start the AR session
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        StopButton.isEnabled = false
+        addTargetNodes()
+        StopButton.isHidden=true //hide stop button..easier to do segue
+        
     }
+    
+    
+    
+    //timer
+    var timer = Timer()
+    var seconds = 15
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(self.updateTime)), userInfo: nil, repeats: true)
+    }
+    @objc func updateTime() {
+        if seconds == 0 {
+            timer.invalidate()
+            removeAudioPlayer()
+            gameOver()
+        }else{
+            seconds -= 1
+            timeLabel.text = "\(seconds)"
+        }
+    }
+    
+    func gameOver(){
+        //go back to game complete
+        performSegue(withIdentifier:"StopSegue", sender: self)
+        
+    }
+    
+    
+    
+    
+    
+    //target
+    func addTargetNodes(){
+        
+        var node = SCNNode()
+        let scene = SCNScene(named: "art.scnassets/shark.dae")
+        node = (scene?.rootNode.childNode(withName: "shark", recursively: true)!)! //press the obj and scenegraph
+        node.scale = SCNVector3(1.0,1.0,1.0)
+        node.name = "monster"
+        node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        node.physicsBody?.isAffectedByGravity = false
+        
+        //position
+        node.position = SCNVector3(0,0,-10)
+        
+        //add to scene
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    
+    
+    func playBGM(){
+        let audioNode = SCNNode()
+        let audioSource = SCNAudioSource(fileNamed: "Sounds/ukulele.mp3")!
+        let audioPlayer = SCNAudioPlayer(source: audioSource)
+        audioNode.addAudioPlayer(audioPlayer)
+        let play = SCNAction.playAudio(audioSource, waitForCompletion: false)
+        audioNode.runAction(play)
+        sceneView.scene.rootNode.addChildNode(audioNode)
+    }
+    func removeAudioPlayer(){
+        sceneView.scene.rootNode.removeAllAudioPlayers()
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -34,38 +108,16 @@ class GameScreen: UIViewController  {
     }
     
     // start data recording and label update timer
-    @IBAction func StartClicked(_ sender: Any) {
-        StopButton.isEnabled = true
-        StartButton.isEnabled = false
-        collector.start()
-        sensor_timer = Timer.scheduledTimer(timeInterval: 1/60, target: self, selector: #selector(self.updateLabels), userInfo: nil, repeats: true)
-    }
-
+    
+    
     // stop data recording and label update timer
     @IBAction func StopClicked(_ sender: Any) {
         collector.end()
-        sensor_timer.invalidate();
+        sceneView.session.pause() //pauses processing in the session
         //performSegue(withIdentifier: "StopSegue", sender: self)
         //self.present(gameComplete, animated: true, completion: nil)
     }
     
-    // timerfunc updates labels with current sensor data
-    @objc fileprivate  func updateLabels(){
-        let curr_data = collector.get_last_entry();
-        let aX = curr_data[0].0
-        let aY = curr_data[0].1
-        let aZ = curr_data[0].2
-        let rX = curr_data[1].0
-        let rY = curr_data[1].1
-        let rZ = curr_data[1].2
-        //Updates the labels
-        self.accelX.text = String(format:"%6.3lf", aX)
-        self.accelY.text = String(format:"%6.3lf", aY)
-        self.accelZ.text = String(format:"%6.3lf", aZ)
-        self.rotateX.text = String(format:"%6.3lf", rX)
-        self.rotateY.text = String(format:"%6.3lf", rY)
-        self.rotateZ.text = String(format:"%6.3lf", rZ)
-        //print(curr_data[0])
-    }
     
 }
+
